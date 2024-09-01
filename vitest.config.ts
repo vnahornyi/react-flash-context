4528aec0e4e69539fd2d8c6ed652094d64685ae9
Vladyslav Nahornyi <vladyslav.nahornyi.dev@gmail.com>
2024-09-01 12:12:51 +0300

    added react 19 rc support

--- vitest.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest" />
-import { defineConfig, mergeConfig } from "vite";
-import viteConfig from "./vite.config";
-
-export default mergeConfig(viteConfig, defineConfig({
-	test: {
-		environment: 'jsdom'
-	}
-}));
